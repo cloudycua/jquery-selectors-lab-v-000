88de5d8e9d0e9666db4ee0df642798f559e50852
Christine Cua <cloudycua@gmail.com>
2019-02-05 19:45:58 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,13 +1,16 @@
 'use-strict';
 
+// all <h1> elements using an element selectro
 function h1Selector(){
   return $('h1')
 }
 
+// all <li> inside of <ol> using a descendant selector
 function liInOlSelector(){
   return $('ol li')
 }
 
+// all <a> inside element that has id 'box4' and class 'box5'
 function linkSelector(){
   return $('#box4.box5 a')
 }
