0ca89824485e1fde5dd9d7435a6f5052ecc4d30b
Christine Cua <cloudycua@gmail.com>
2019-02-05 19:38:58 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 'use-strict';
+
+function h1Selector(){
+  return $('')
+}
+
+function liIn01Selector(){
+  return $('')
+}
+
+function linkSelector(){
+  return $('')
+}
+
+function imageSelector(){
+  return $('')
+}
+
+function checkboxInputSelector(){
+  return $('')
+}
